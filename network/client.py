0c529b32a0fcc4c863a445b3fffff4567c8f7c63
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-06-23 14:23:36 +0200

    updating networking

--- network/client.py
+++ network/client.py
@@ -6,7 +6,7 @@ import socket
 TCP_IP = '192.168.1.106'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = socket.gethostname()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
--- network/server.py
+++ network/server.py
@@ -2,20 +2,23 @@
 
 import socket
 
-
 TCP_IP = '192.168.1.106'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-conn, addr = s.accept()
-print('Connection address:', addr)
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print("received data:", data)
-    conn.send(data)  # echo
-conn.close()
+while True:
+    conn, addr = s.accept()
+    print('Connection address:', addr)
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        print("received data:", data)
+        f = open("ips.txt", "a")
+        f.write("\n" + addr[0] + "\t" + data)
+        f.close()
+        conn.send(data)
+    conn.close()
