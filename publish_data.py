5f60eec5173b8b1678b8c22bfbd966a521653c74
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-06-23 09:37:37 +0200

    minor updates for transmission

--- publish_data.py
+++ publish_data.py
@@ -5,6 +5,7 @@ pub = rospy.Publisher('/test', String, queue_size=10)
 rospy.init_node('publisher1')
 r = rospy.Rate(30) # 10hz
 while not rospy.is_shutdown():
-   time = str(rospy.get_rostime().secs) + "." + str(rospy.get_rostime().nsecs)
+   ros_time = rospy.get_rostime()
+   time = str(now.secs) + "." + str(now.nsecs)
    pub.publish(time)
    r.sleep()
--- recieve_data.py
+++ recieve_data.py
@@ -3,35 +3,10 @@ from std_msgs.msg import String
 import subprocess
 import std_msgs
 
-def get_time_difference():
-    # Gets the local time from each machine with a ROS call
-    data = subprocess.check_output("rosservice call /master_discovery/list_masters | grep -iw 'name:\|timestamp_local'", shell=True).replace("\"", "").split("\n")
-
-    # Removes whitespaces and other stuff to only get the actual time.
-    print(data)
-    data_clean = []
-    for i in data[:-1]:
-        data_clean.append((i.split(": ")[1]))
-    print(data_clean)
-
-    times = []
-    names = []
-    for i in range(0, len(data_clean)):
-        if i % 2 == 0:
-            names.append(data_clean[i])
-        else:
-            times.append(float(data_clean[i]))
-    print(names, times)
-    # Calculate the difference from host (the first time is the host)
-    time_diff = []
-    for i in times:
-        time_diff.append(times[0] - i)
-    print(time_diff)
-    return time_diff
-
 def callback(data):
-    current_time = str(rospy.get_rostime().secs) + "." + str(rospy.get_rostime().nsecs)
-    rospy.loginfo("I heard %s current_time: %d", data.data, float(str(current_time)) - float(data.data))
+    now = rospy.get_rostime()
+    now_str = str(now.secs) + "." + str(now.nsecs)
+    rospy.loginfo("Sent at %s Current time: %s \t difference: %s", data.data, now_str, str(float(now_str) - float(data.data)))
     #rospy.loginfo("I heard %s", data.machine)
     
 def listener():
