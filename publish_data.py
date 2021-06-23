c025b0cc70e65a32c64d7fb280b87ddd469bef20
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-06-23 09:50:19 +0200

    testing listeners

--- publish_data.py
+++ publish_data.py
@@ -1,11 +1,13 @@
 import rospy
 from std_msgs.msg import String
+from rospy_tutorials.msg import HeaderString
 
 pub = rospy.Publisher('/test', String, queue_size=10)
 rospy.init_node('publisher1')
 r = rospy.Rate(30) # 10hz
+
 while not rospy.is_shutdown():
-   ros_time = rospy.get_rostime()
+   now = rospy.get_rostime()
    time = str(now.secs) + "." + str(now.nsecs)
    pub.publish(time)
    r.sleep()
