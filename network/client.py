911363af553bd1998154536ab1b037cf6c0de2fc
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-06-24 09:55:05 +0200

    preparing for broadcasting

--- network/client.py
+++ network/client.py
@@ -11,7 +11,8 @@ MESSAGE = socket.gethostname()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
 
-print("received data:", data)
+while True:
+    data = s.recv(BUFFER_SIZE)
+    print("received data:", data)
+s.close()
