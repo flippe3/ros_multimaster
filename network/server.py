e7d23ad95f4b17a00c5d3bc823cc08e41271e4af
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-06-28 16:38:42 +0200

    fix broadcasting

--- network/server.py
+++ network/server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import socket
 import threading
 
@@ -15,41 +14,48 @@ s.listen(1)
 # Wipe ip file on boot
 open('ips.txt', 'w').close()
 
+def overwrite_host(ip_file, new_ip):
+    lines = ips.split("\n")
+    foverwrite = open("ips.txt", "w")
+    for line in lines:
+        if addr[0] in line:
+            foverwrite.write(ips.replace(line, new_ip))
+    foverwrite.close()
+
+def update_ip_file(conn,addr,data):
+    fin = open("ips.txt", "r")
+    ips = fin.read()
+    new_ip = "\n"+addr[0]+"\t"+data
+    fout = open("ips.txt", "a")
+    fin.close()
+    
+    if addr[0] in ips:
+        if new_ip in ips:
+            print("IP already added.")
+        else:
+            overwrite_host(ips, new_ip)
+    else:
+        fout.write(new_ip)
+        fout.close()
+            
+    fin = open("ips.txt", "r")
+    ips = fin.read()
+    conn.send(ips)
+    fin.close()
+
+    
 def on_new_client(conn, addr):
     while True:
         data = conn.recv(BUFFER_SIZE)
         if not data: break
         print("received data:", data)
 
-        # Makes sure that the data is not replicated
-        fin = open("ips.txt", "r")
-        ips = fin.read()
-        new_ip = "\n"+addr[0]+"\t"+data
-        fout = open("ips.txt", "a")
-        fin.close()
-        
-        if addr[0] in ips:
-            if new_ip in ips:
-                fout.write(new_ip)
-            else:
-                lines = ips.split("\n")
-                foverwrite = open("ips.txt", "w")
-                for line in lines:
-                    if addr[0] in line:
-                        foverwrite.write(ips.replace(line, new_ip))
-                foverwrite.close()
-        else:
-            fout.write(new_ip)
-        fout.close()
-        
-        fin = open("ips.txt", "r")
-        ips = fin.read()
-        conn.send(ips)
-        fin.close()
-#    conn.close()
+        update_ip_file(conn,addr,data)
+    conn.close()
     
 ip_list = []
 threads = []
+
 while True:
     c, addr = s.accept()
     ip_list.append(addr[0])
@@ -59,4 +65,8 @@ while True:
     t = threading.Thread(target=on_new_client,args=(c,addr))
     t.start()
     threads.append(t)
+    print(threads)
+    # IF A NEW THREAD COMES IN THE OLD THREADS NEEDS TO BE UPDATED.
+
+    
 s.close()
