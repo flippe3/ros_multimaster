0619cb0334f90a890d64cb8046aedad43b20a315
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-06-29 05:37:14 +0200

    client works but does not check duplicates

--- network/client.py
+++ network/client.py
@@ -12,7 +12,17 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send(str.encode(MESSAGE))
 
+def check_duplicates(data):
+    ips = data.split("\n")
+    hosts = open("/etc/hosts", "r")
+    print(ips)
+    for i in ips:
+        if i in hosts.read():
+            print("duplicates")            
+    hosts.close()
 while True:
     data = s.recv(BUFFER_SIZE)
     print("received data:", data)
+    hosts = open("/etc/hosts", "a")
+    hosts.write(data)
 s.close()
