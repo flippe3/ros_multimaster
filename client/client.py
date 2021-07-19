99f09ba666b316431c604b379e99973ae59916f9
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-07-19 11:38:07 +0200

    client setup

--- client/client.py
+++ client/client.py
@@ -1,11 +1,21 @@
 import sys
 sys.path.append('util')
 from multimaster import Multimaster
+import os
 
 class Client:
-    def __init__(self):
+    def __init__(self, host_ip):
+        self.first_setup(host_ip)
+        self.setup()
+        
+    def first_setup(self, host_ip):
+        os.system("export ROS_MASTER_URI=http://" + str(host_ip) + ":11311")
+        os.system("sudo sh -c 'echo net.ipv4.icmp_echo_ignore_broadcasts=0 >> /etc/sysctl.conf'")
+        os.system("sudo service procps restart")
+        
+    def setup(self):
         multimaster = Multimaster()
         multimaster.setup()
-
+        
 # for debugging
-client = Client()
+client = Client(192.168.0.57)
