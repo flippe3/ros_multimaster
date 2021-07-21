8ab5d196a5e7acfcbb68ac9c4d5c656eef02a8e9
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-07-21 14:21:32 +0200

    changed because the setup needs to be run in sudo

--- client/client.py
+++ client/client.py
@@ -1,38 +1,12 @@
 import sys
 sys.path.append('util')
 from multimaster import Multimaster
-import os
-import socket
-from wipe import cleanup_hosts
 
 class Client:
-    def __init__(self, host_ip, interface):
-        cleanup_hosts()
-        self.first_setup(host_ip, interface)
-        self.start_multimaster()
-        
-    def first_setup(self, host_ip, interface):
-        try:
-            os.system("export ROS_MASTER_URI=http://" + str(host_ip) + ":11311")
-            ip = os.popen('ip addr show ' + interface).read().split("inet ")[1].split("/")[0]
+    def __init__(self):
+        self.multimaster = Multimaster()
+        self.multimaster.setup(debug=True)
 
-            hosts = open("/etc/hosts", "a")
-            hosts.write("### AUTOMATED NETWORK CONFIG ###\n")
-            hosts.write(str(host_ip) + "\thost\n")
-            hosts.write(str(ip) + "\t" + str(socket.gethostname()))
-            hosts.write("\n### END ###\n")
 
-            os.system("export ROS_HOSTNAME=" + str(ip))
-            os.system("sudo sh -c 'echo net.ipv4.icmp_echo_ignore_broadcasts=0 >> /etc/sysctl.conf'")
-            os.system("sudo service procps restart")
+c = Client()
 
-        except IOError:
-            print("ERROR, are you running this as root?")
-            sys.exit()
-            
-    def start_multimaster(self):
-        multimaster = Multimaster()
-        multimaster.setup()
-        
-# for debugging
-client = Client(host_ip="192.168.0.201", interface="wlp59s0")
--- client/client_setup.py
+++ client/client_setup.py
@@ -1,25 +1,24 @@
 import sys
 sys.path.append('util')
-from multimaster import Multimaster
 import os
 import socket
-import wipe
+from wipe import cleanup_hosts
 
-class Client:
+class Client_Setup:
     def __init__(self, host_ip, interface):
-        
+        cleanup_hosts()
         self.first_setup(host_ip, interface)
-        self.setup()
-        
+
     def first_setup(self, host_ip, interface):
         try:
             os.system("export ROS_MASTER_URI=http://" + str(host_ip) + ":11311")
             ip = os.popen('ip addr show ' + interface).read().split("inet ")[1].split("/")[0]
 
             hosts = open("/etc/hosts", "a")
-            hosts.write("\n### AUTOMATED NETWORK CONFIG ###\n")
+            hosts.write("### AUTOMATED NETWORK CONFIG ###\n")
             hosts.write(str(host_ip) + "\thost\n")
             hosts.write(str(ip) + "\t" + str(socket.gethostname()))
+            hosts.write("\n### END ###\n")
 
             os.system("export ROS_HOSTNAME=" + str(ip))
             os.system("sudo sh -c 'echo net.ipv4.icmp_echo_ignore_broadcasts=0 >> /etc/sysctl.conf'")
@@ -29,9 +28,5 @@ class Client:
             print("ERROR, are you running this as root?")
             sys.exit()
             
-    def setup(self):
-        multimaster = Multimaster()
-        multimaster.setup()
-        
 # for debugging
-client = Client(host_ip="192.168.0.57", interface="wlp59s0")
+c = Client_Setup(host_ip="192.168.0.201", interface="wlp59s0")
