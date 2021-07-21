8ee1da6d8e9a375adcc3f64791a2cea6786efc63
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-07-21 10:40:46 +0200

    client setup

--- util/wipe.py
+++ util/wipe.py
@@ -1,10 +1,20 @@
 import os
 
 def cleanup_hosts():
-    hosts = open("/etc/hosts", 'rw')
+    hosts = open("/etc/hosts", 'r')
     start = "### AUTOMATED NETWORK CONFIG ###"
+    end = "### END ###"
+    new_file = ""
+    found = False
     for line in hosts.readlines():
-        print(line)
         if start in line:
-            
-cleanup_hosts()
+            found = True
+        elif found == False:
+            new_file += line
+        elif found == True and end in line:
+            found = False
+    hosts.close()
+    new_hosts = open("/etc/hosts", 'w')
+    new_hosts.write(new_file)
+    new_hosts.close()
+    
