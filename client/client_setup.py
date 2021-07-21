63bd1a7c0ff08a7e1c72b35448894f2d1675cf72
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-07-21 14:31:10 +0200

    wlan interface

--- client/client_setup.py
+++ client/client_setup.py
@@ -7,16 +7,20 @@ from wipe import cleanup_hosts
 class Client_Setup:
     def __init__(self, host_ip, interface):
         cleanup_hosts()
-        self.first_setup(host_ip, interface)
 
-    def first_setup(self, host_ip, interface):
+        self.host_ip = host_ip
+        self.interface = interface
+        self.first_setup(host_ip, interface)
+        self.connect_server()
+        
+    def first_setup(self)
         try:
-            os.system("export ROS_MASTER_URI=http://" + str(host_ip) + ":11311")
-            ip = os.popen('ip addr show ' + interface).read().split("inet ")[1].split("/")[0]
+            os.system("export ROS_MASTER_URI=http://" + str(self.host_ip) + ":11311")
+            ip = os.popen('ip addr show ' + self.interface).read().split("inet ")[1].split("/")[0]
 
             hosts = open("/etc/hosts", "a")
             hosts.write("### AUTOMATED NETWORK CONFIG ###\n")
-            hosts.write(str(host_ip) + "\thost\n")
+            hosts.write(str(self.host_ip) + "\thost\n")
             hosts.write(str(ip) + "\t" + str(socket.gethostname()))
             hosts.write("\n### END ###\n")
 
@@ -27,6 +31,13 @@ class Client_Setup:
         except IOError:
             print("ERROR, are you running this as root?")
             sys.exit()
+        except IndexError:
+            print("ERROR, have you configured the correct interface?")
+            sys.exit()
             
+
+    def connect_server(self):
+        return 0
+
 # for debugging
-c = Client_Setup(host_ip="192.168.0.201", interface="wlp59s0")
+c = Client_Setup(host_ip="192.168.0.201", interface="wlan0")
