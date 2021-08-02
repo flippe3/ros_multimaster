0b0ef5b3404dec760d067475a78a353fcd19e450
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-08-02 11:21:19 +0200

    started integrating backend with frontend

--- webserver/flask/base.py
+++ webserver/flask/base.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
 from flask import render_template
 
+import sys
+sys.path.append('../../server')
+sys.path.append('../../util')
+from terminal import Terminal
+
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+terminal = Terminal()
 
 @app.route("/")
 @app.route("/index.html")
@@ -20,3 +24,13 @@ def server_command():
     server_input = request.form['input_server']
     print("[Recieved server input] :", server_input)
     return render_template('index.html')
+
+@app.route("/machine/", methods=['POST'])
+def machine_command():
+    machine_input = request.form['input_machine']
+    print("[Recieved machine input] :", machine_input)
+    terminal.send_cmd(machine_input)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
