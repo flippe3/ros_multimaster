327a0ca2918ae8b4b7156f58dc044826c7d1c62f
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-08-02 10:54:00 +0200

    working on web terminal

--- webserver/flask/base.py
+++ webserver/flask/base.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 @app.route("/")
 @app.route("/index.html")
 def base(name=None):
-    return render_template('base.html', name=name)
+    return render_template('index.html', name=name)
 
 @app.route("/control.html")
 def hello(name=None):
     return render_template('control.html', name=name)
 
-app.run(debug=True)
+@app.route("/server/", methods=['POST'])
+def server_command():
+    server_input = request.form['input_server']
+    print("[Recieved server input] :", server_input)
+    return render_template('index.html')
