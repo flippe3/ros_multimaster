95260cfa613a5fc4c466e2057b8a4f9dc202d83b
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-08-02 18:04:45 +0200

    web stuff

--- webserver/flask/base.py
+++ webserver/flask/base.py
@@ -16,8 +16,13 @@ def base(name=None):
     return render_template('index.html', name=name)
 
 @app.route("/control.html")
-def hello(name=None):
+def control(name=None):
     return render_template('control.html', name=name)
+
+@app.route("/connection.html")
+def connection(name=None):
+    return render_template('connection.html', name=name)
+
 
 @app.route("/server/", methods=['POST'])
 def server_command():
@@ -25,7 +30,7 @@ def server_command():
     print("[Recieved server input] :", server_input)
     output = terminal.send_cmd(server_input)
     print("Recieved output:", output)
-    return render_template('index.html', value=output)
+    return render_template('index.html', server_output=output)
 
 @app.route("/machine/", methods=['POST'])
 def machine_command():
@@ -33,6 +38,26 @@ def machine_command():
     print("[Recieved machine input] :", machine_input)
     terminal.send_cmd(machine_input)
     return render_template('index.html')
+
+@app.route("/connect/", methods=['POST'])
+def connection_command():
+    ip_address = request.form['connection_ip']
+    port = request.form['connection_port']
+    print("[Recieved IP] :", str(ip_address), ":", str(port))
+    command = "connect " + str(ip_address).strip() + " " + str(port).strip()
+    output = terminal.send_cmd(command)
+    return render_template('/connection.html', connection=output)
+
+@app.route("/list-connections/", methods=['POST'])
+def list_connections_command():
+    output = terminal.send_cmd("list sockets")
+    return render_template('/connection.html', list_connections=output)
+
+@app.route("/list-connections/index/", methods=['POST'])
+def list_connections_index_command():
+    output = terminal.send_cmd("list sockets")
+    return render_template('/index.html', list_output=output)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
