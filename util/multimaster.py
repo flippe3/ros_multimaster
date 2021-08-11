edec1f64f2ccd8b973f318888626cf3bf514111d
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-08-11 22:24:57 +0200

    updated multimaster

--- util/multimaster.py
+++ util/multimaster.py
@@ -1,22 +1,35 @@
 import sys
 sys.path.append('util')
-from process_mgmt import Subprocess
+#from process_mgmt import Subprocess
+import subprocess, time
+import threading
 
 class Multimaster:
-    def setup(self, debug=False):
-        print("[INFO] Starting multimaster")
-        # setting up the subprocesses
-        self.master_discovery_fkie = Subprocess("rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1") 
-        self.master_sync_fkie = Subprocess("rosrun master_sync_fkie master_sync")
+    def __init__(self):
+        self.roscore = threading.Thread(target=self.start_ros)
+        print("[INFO] Starting roscore")
+        self.roscore.start()
+        time.sleep(1)
+        
+        self.discovery = threading.Thread(target=self.multimaster_discovery)
+        print("[INFO] Starting multimaster discovery")
+        self.discovery.start()
+        time.sleep(1)
 
-        # runs the terminal command
-        self.master_discovery_fkie.run(output=debug, service=True)
-        self.master_sync_fkie.run(output=debug, service=True)
-        print("[INFO] Successfully started multimaster")
+        self.sync = threading.Thread(target=self.multimaster_sync)
+        print("[INFO] Starting multimaster sync")
+        self.sync.start()
+        time.sleep(1)
+        
+    def start_ros(self):
+        subprocess.call(["roscore"], shell=True)
+        
+    def multimaster_discovery(self):
+        subprocess.call(["rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1"], shell=True)
 
-    def terminate(self):
-        self.master_discovery_fkie.terminate()
-        self.master_sync_fkie.terminate()
-        print("[INFO] Terminated multimaster")
+    def multimaster_sync(self):
+        subprocess.call(["rosrun master_sync_fkie master_sync"], shell=True)
+
 
     
+m = Multimaster()
