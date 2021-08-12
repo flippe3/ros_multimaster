17679eb9e3dae761c93e6e14c45d001270d6bdcf
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-08-12 21:11:15 +0200

    client with port entry

--- client/client.py
+++ client/client.py
@@ -8,16 +8,16 @@ from command_server import Command_Server
 import threading, time
 
 class Client:
-    def __init__(self, multimaster=True, roscore=True):
+    def __init__(self, port=5006):
+        self.multimaster = Multimaster() # Starts roscore and multimaster
         self.setup_server()
         
-    def setup_server(self):
-        self.server = Command_Server(port=5007)
+    def setup_server(self, port):
+        self.server = Command_Server(port=port)
         self.server.start()
 
-    #def start_ros(self):
-    #    p = Subprocess("roscore")
-    #    p.run(output=False, service=True)
-    #    return p 
-
-c = Client()
+try:
+    c = Client(port=sys.argv[1])
+except:
+    print("Error did you start the program correctly?")
+    print("Example: python3 client/client.py 5006")
